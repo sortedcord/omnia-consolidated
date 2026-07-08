17c5b95f3bbc248fea56e35418af77567dd29369
Aditya Gupta <mail@adityagupta.dev>
2026-07-09 05:07:25 +0530

    update: docs

--- web/docs/astro.config.mjs
+++ web/docs/astro.config.mjs
@@ -1,20 +1,23 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import mermaid from "astro-mermaid";
 
 export default defineConfig({
   site: "https://omnia.omniasimulation.com",
   base: "/docs",
   integrations: [
+    mermaid(),
     starlight({
       title: "Omnia Docs",
       logo: {
         src: "./src/assets/img/logo.png",
+        replacesTitle: true,
       },
-      social: [{ icon: "github", label: "GitHub", href: "https://github.com/sortedcord/omnia" }],
+      social: [{ icon: "github", label: "GitHub", href: "https://github.com/sortedcord/omnia-consolidated" }],
       sidebar: [
         {
           label: "Introduction",
-          slug: "intro",
+          slug: "index",
         },
         {
           label: "Architecture",
